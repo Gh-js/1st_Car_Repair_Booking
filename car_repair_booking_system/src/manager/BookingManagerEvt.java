3bc9cda3c0c46212cb7b9760f493084a81211b4d
DESKTOP-COJ9858\user <dkrnlqkqn586@naver.com>
2023-08-31 17:34:28 +0900

    Booking으로 변경

--- car_repair_booking_system/src/manager/BookingManagerEvt.java
+++ car_repair_booking_system/src/manager/BookingManagerEvt.java
@@ -3,7 +3,7 @@ package manager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class ReservationManagerEvt implements ActionListener {
+public class BookingManagerEvt implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
